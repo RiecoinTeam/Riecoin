3ddab9dacdfd93fe17399b874360a84f235eae2c
Pttn <28868425+Pttn@users.noreply.github.com>
2019-04-27 15:18:22 +0200

    Increase threshold to 80%, window to 2 weeks
    Signed-off-by: Pttn <28868425+Pttn@users.noreply.github.com>

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -89,8 +89,8 @@ public:
         consensus.nPowTargetSpacing = 2.5 * 60;
         consensus.fPowAllowMinDifficultyBlocks = false;
         consensus.fPowNoRetargeting = false;
-        consensus.nRuleChangeActivationThreshold = 864; // 75%
-        consensus.nMinerConfirmationWindow = 1152; // 48 h
+        consensus.nRuleChangeActivationThreshold = 6452; // 80%
+        consensus.nMinerConfirmationWindow = 8064; // 14 days
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].bit = 28;
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nStartTime = 1199145601; // January 1, 2008
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nTimeout = 1230767999; // December 31, 2008
